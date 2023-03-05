28ac284fa0d817211cb328ce65cff9a5e6ca7e18
Varma <anjivarma.penmatsa@gmail.com>
2023-03-05 10:46:55 +0530

    Initial Commit

---
+++ Swift-Ui_learnings/Swift_Ui_learningsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Ui_learningsApp.swift
+//  Swift-Ui_learnings
+//
+//  Created by Penumatsa Anjaneya varma on 05/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Ui_learningsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
